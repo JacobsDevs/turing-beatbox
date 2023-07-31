46a96dcd25971d123e4f10074df2dff04eb7c9c1
Jacob <jacobphelan@hotmail.com>
2023-07-31 12:08:07 +1000

    Add handling of multiple nodes

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,23 +1,40 @@
 require './lib/node'
 
 class LinkedList
-  attr_reader :head
+  attr_reader :head, :tail
 	def initialize(first_component = nil)
 	  @head = first_component
+		@tail = nil
 	end
 
 	def append(data)
 		node = Node.new(data)
-		@head = node if @head == nil
+		if @head == nil
+			@head = node
+			@tail = @head
+		else
+			@tail.next_node = node
+			@tail = node
+		end
 	end
 
 	def count
-		counter = 0
-		return counter if @head == nil
-		return 1
+		counter = 1
+		current_node = @head
+		while current_node.next_node != nil
+		  current_node = current_node.next_node
+		  counter += 1
+		end
+    counter
 	end
 
 	def string
-		return @head.data
+		string = ""
+		current_node = @head
+		count.times do
+			string += "#{current_node.data} "
+			current_node = current_node.next_node
+		end
+		string.strip
 	end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -22,10 +22,28 @@ describe LinkedList do
 	it 'displays next node in list' do
     expect(@linked_list.head.next_node).to eq(nil)
 	end
+	
 	it 'counts nodes' do
 		expect(@linked_list.count).to eq(1)
 	end
-	it 'generates a string containing all nodes' do
+	
+	it 'generates a string containing node data' do
     expect(@linked_list.string).to eq("doop")
+	end
+  
+	it 'append additional nodes' do
+		@linked_list.append("boop")
+	  expect(@linked_list.tail.data).to eq("boop") 
+		expect(@linked_list.head.next_node.data).to eq("boop")
+	end
+
+	it 'counts multiple nodes' do
+		@linked_list.append("boop")
+	  expect(@linked_list.count).to eq(2)
+	end
+
+	it 'generates a string containing all nodes data' do
+	  @linked_list.append("boop")
+	  expect(@linked_list.string).to eq("doop boop")
 	end
 end
